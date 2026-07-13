17cb8a0f3d14a52f4434a93518a8f21233ba031f
sajad-afzali-me <sajad.afzali.me@gmail.com>
2026-07-13 22:15:22 +0330

    organize exercises

--- even_odd.py
+++
@@ -1,9 +0,0 @@
-num = float(input("Enter a number\n n: "))
-if not num.is_integer():
-    print("ERROR: Odd or Even is only for integer numbers , not decimals")
-else:
-    num = int(num)
-    if num % 2 == 0:
-        print(f"{num} is Even")
-    else:
-        print(f"{num} is Odd")
